bf8b949085e9a9b5f75b11508dba3b5340bdc74f
abdulbasit <habdulbasithassan77@gmail.com>
2023-05-18 04:55:28 +0300

    finished

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name="programmer")
+
+  puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+  return num1 + num2
+end
+
+def halve(num1)
+   if num1.class !=Interger
+    return nil
+   else 
+    return num /2
+   end
+end
+
+
